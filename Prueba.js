c86eb7940e7424007a36e3a8df6e007bdcf25e45
Miguel <miguel17693@hotmail.com>
2020-01-17 14:43:29 +0100

    vamos

--- Prueba.js
+++ Prueba.js
@@ -1,5 +1,6 @@
-var impares= []
-var pares = []
+
+// es una función donde estran 2 variables y salen rellenas de 50 números dividios en dos arrays de pares e impares
+function generaAleatorioPareseImpares (impares, pares) {
 
 for (var i=0; i<50; i++){
 	numeroAzar= Math.floor((Math.random()*100)+1)
@@ -9,6 +10,12 @@ for (var i=0; i<50; i++){
 	}
 	else impares.push(numeroAzar)
 }
+}
 
-console.log(impares)
-console.log(pares)
+var array1=[]
+var array2=[]
+//tengo que darle dos arrays
+generaAleatorioPareseImpares(array1,array2)
+
+console.log(array1)
+console.log(array2)
